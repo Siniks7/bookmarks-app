06f1c14b9a15457c1090dd4ec5e783e2048f0a2d
Nikolay <siniks7@yandex.ru>
2025-10-27 12:16:29 +0700

    Установка Pinia

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,9 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import './assets/main.css'
+import { createApp } from 'vue';
+import App from './App.vue';
+import { createPinia } from 'pinia';
+import './assets/main.css';
 
-createApp(App).mount('#app')
+const pinia = createPinia();
+const app = createApp(App);
+app.use(pinia);
+app.mount('#app');
