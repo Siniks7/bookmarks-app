ce0b053425a1e61e7386e139481592e9107022da
Nikolay <siniks7@yandex.ru>
2025-11-10 16:42:44 +0700

    Установка Vue Router

--- src/main.ts
+++ src/main.ts
@@ -2,8 +2,15 @@ import { createApp } from 'vue';
 import App from './App.vue';
 import { createPinia } from 'pinia';
 import './assets/main.css';
+import { createRouter, createWebHistory } from 'vue-router';
+
+const router = createRouter({
+  routes: [],
+  history: createWebHistory(),
+});
 
 const pinia = createPinia();
 const app = createApp(App);
 app.use(pinia);
+app.use(router);
 app.mount('#app');
