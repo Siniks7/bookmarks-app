d3671105beec9683ed6d70fb6cda7550fda56b1e
Nikolay <siniks7@yandex.ru>
2025-11-10 17:13:28 +0700

    Упражнение - Страница входа

--- src/views/AuthView.vue
+++ src/views/AuthView.vue
@@ -1,1 +1,31 @@
-<template>Авторизация</template>
+<script setup lang="ts">
+import ButtonText from '@/components/ButtonText.vue';
+</script>
+
+<template>
+  <div class="auth">
+    <div class="auth__form">
+      <h1 class="auth__header">Bookmarkly</h1>
+      <ButtonText>Вход</ButtonText>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.auth {
+  display: grid;
+  place-items: center;
+  min-height: 100vh;
+}
+.auth__header {
+  font-size: 52px;
+  font-weight: 700;
+}
+.auth__form {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  gap: 52px;
+}
+</style>
