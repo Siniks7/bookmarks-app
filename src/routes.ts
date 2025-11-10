4a4ffcb596f72b98f59cf9f2627fe13d5d0234d5
Nikolay <siniks7@yandex.ru>
2025-11-10 17:22:06 +0700

    LazyLoading

--- src/routes.ts
+++ src/routes.ts
@@ -1,18 +1,17 @@
 import { createRouter, createWebHistory } from 'vue-router';
-import TestComp from './components/TestComp.vue';
-import TestComp2 from './components/TestComp2.vue';
-import MainView from './views/MainView.vue';
-import AuthView from './views/AuthView.vue';
 
 export const router = createRouter({
   routes: [
-    { path: '/', component: AuthView },
+    {
+      path: '/',
+      component: () => import('./views/AuthView.vue'),
+    },
     {
       path: '/main',
-      component: MainView,
+      component: () => import('./views/MainView.vue'),
       children: [
-        { path: '', component: TestComp, name: 'main' },
-        { path: 'new', component: TestComp2 },
+        { path: '', component: () => import('./components/TestComp.vue'), name: 'main' },
+        { path: 'new', component: () => import('./components/TestComp2.vue') },
       ],
     },
   ],
