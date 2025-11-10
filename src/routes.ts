a2e4a3768fd89a2ec4b9d34e6c54c6b24807e612
Nikolay <siniks7@yandex.ru>
2025-11-10 17:08:42 +0700

    fix children

--- src/routes.ts
+++ src/routes.ts
@@ -1,16 +1,19 @@
 import { createRouter, createWebHistory } from 'vue-router';
 import TestComp from './components/TestComp.vue';
 import TestComp2 from './components/TestComp2.vue';
+import MainView from './views/MainView.vue';
+import AuthView from './views/AuthView.vue';
 
 export const router = createRouter({
   routes: [
+    { path: '/', component: AuthView },
     {
-      path: '/',
-      component: TestComp,
-    },
-    {
-      path: '/new',
-      component: TestComp2,
+      path: '/main',
+      component: MainView,
+      children: [
+        { path: '', component: TestComp },
+        { path: 'new', component: TestComp2 },
+      ],
     },
   ],
   history: createWebHistory(),
