fe9a4deaa261d295ac8877c580f1219ae45ca784
Nikolay <siniks7@yandex.ru>
2025-11-11 10:43:50 +0700

    Упражнение - Получение закладок

--- src/views/CategoryView.vue
+++ src/views/CategoryView.vue
@@ -1,25 +1,30 @@
 <script setup lang="ts">
-import { onBeforeRouteUpdate, useRoute } from 'vue-router';
+import { useRoute } from 'vue-router';
+import { useBookmarkStore } from '@/stores/bookmark.store';
 import type { Category } from '@/interfaces/category.inteface';
 import { useCategoryStore } from '@/stores/categories.store';
 import { ref, watch } from 'vue';
 const route = useRoute();
-const state = useCategoryStore();
+const categoryStore = useCategoryStore();
+const bookmarkStore = useBookmarkStore();
 const category = ref<Category>();
 
 watch(
-  () => state.categories,
-  () => {
-    category.value = state.getCategoryByAlias(route.params.alias as string);
+  () => ({
+    alias: route.params.alias,
+    categories: categoryStore.categories,
+  }),
+  (data) => {
+    category.value = categoryStore.getCategoryByAlias(data.alias as string);
+    if (category.value) {
+      bookmarkStore.fetchBookmarks(category.value.id);
+    }
   },
 );
-
-onBeforeRouteUpdate((to) => {
-  category.value = state.getCategoryByAlias(to.params.alias as string);
-});
 </script>
 
 <template>
   Category
   {{ category?.name }}
+  {{ bookmarkStore.bookmarks.length }}
 </template>
