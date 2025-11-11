32221e34b0f45fef23a588b763a75d34042ba160
Nikolay <siniks7@yandex.ru>
2025-11-11 10:31:23 +0700

    Helpers

--- src/views/CategoryView.vue
+++ src/views/CategoryView.vue
@@ -1,11 +1,24 @@
 <script setup lang="ts">
 import { useRoute } from 'vue-router';
-
+import type { Category } from '@/interfaces/category.inteface';
+import { useCategoryStore } from '@/stores/categories.store';
+import { ref, watch } from 'vue';
 const route = useRoute();
-console.log(route.params.alias);
+const state = useCategoryStore();
+const category = ref<Category>();
+
+watch(
+  () => ({
+    alias: route.params.alias,
+    categories: state.categories,
+  }),
+  (data) => {
+    category.value = state.getCategoryByAlias(data.alias as string);
+  },
+);
 </script>
 
 <template>
   Category
-  {{ $route.params.alias }}
+  {{ category?.name }}
 </template>
